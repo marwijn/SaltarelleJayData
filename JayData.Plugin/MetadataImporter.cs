1a8f9d947d0efbc9ea2d3e1634b85424d61a17fe
Marwijn <Marwijn@hotmail.com>
2013-12-10 08:59:59 +0100

    added test project

--- JayData.Plugin/MetadataImporter.cs
+++ JayData.Plugin/MetadataImporter.cs
@@ -1,74 +1,80 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using ICSharpCode.NRefactory.TypeSystem;
-//using JayDataApi;
-//using Saltarelle.Compiler;
-//using Saltarelle.Compiler.Compiler;
-//using Saltarelle.Compiler.Decorators;
-//using Saltarelle.Compiler.JSModel.Expressions;
-//using Saltarelle.Compiler.ScriptSemantics;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ICSharpCode.NRefactory.TypeSystem;
+using JayDataApi;
+using Saltarelle.Compiler;
+using Saltarelle.Compiler.Compiler;
+using Saltarelle.Compiler.Decorators;
+using Saltarelle.Compiler.JSModel.Expressions;
+using Saltarelle.Compiler.JSModel.TypeSystem;
+using Saltarelle.Compiler.ScriptSemantics;
 
-//namespace JayData.Plugin
-//{
-//    public class MetadataImporter : MetadataImporterDecoratorBase, IRuntimeContext
-//    {
-//        private readonly IErrorReporter _errorReporter;
-//        private readonly IRuntimeLibrary _runtimeLibrary;
-//        private readonly INamer _namer;
-//        private readonly bool _minimizeNames;
+namespace JayData.Plugin
+{
+    public class MetadataImporter : MetadataImporterDecoratorBase, IRuntimeContext, IJSTypeSystemRewriter
+    {
+        private readonly IErrorReporter _errorReporter;
+        private readonly IRuntimeLibrary _runtimeLibrary;
+        private readonly INamer _namer;
+        private readonly bool _minimizeNames;
 
-//        public MetadataImporter(IMetadataImporter prev, IErrorReporter errorReporter, IRuntimeLibrary runtimeLibrary, INamer namer, CompilerOptions options)
-//            : base(prev)
-//        {
-//            _errorReporter = errorReporter;
-//            _runtimeLibrary = runtimeLibrary;
-//            _namer = namer;
-//            _minimizeNames = options.MinimizeScript;
-//        }
+        public MetadataImporter(IMetadataImporter prev, IErrorReporter errorReporter, IRuntimeLibrary runtimeLibrary, INamer namer, CompilerOptions options)
+            : base(prev)
+        {
+            _errorReporter = errorReporter;
+            _runtimeLibrary = runtimeLibrary;
+            _namer = namer;
+            _minimizeNames = options.MinimizeScript;
+        }
 
 
-//        private bool? IsAutoProperty(IProperty property)
-//        {
-//            if (property.Region == default(DomRegion))
-//                return null;
-//            return property.Getter != null && property.Setter != null && property.Getter.BodyRegion == default(DomRegion) && property.Setter.BodyRegion == default(DomRegion);
-//        }
+        private bool? IsAutoProperty(IProperty property)
+        {
+            if (property.Region == default(DomRegion))
+                return null;
+            return property.Getter != null && property.Setter != null && property.Getter.BodyRegion == default(DomRegion) && property.Setter.BodyRegion == default(DomRegion);
+        }
 
-//        public override void Prepare(ITypeDefinition type)
-//        {
-//            if (AttributeReader.HasAttribute<EntityAttribute>(type))
-//            {
+        public override void Prepare(ITypeDefinition type)
+        {
+            if (AttributeReader.HasAttribute<EntityAttribute>(type))
+            {
 
-//                foreach (var p in type.Properties.Where(p => IsAutoProperty(p) == true))
-//                {
-//                    base.ReserveMemberName(p.DeclaringTypeDefinition, p.Name, false);
-//                    base.SetPropertySemantics(p, PropertyScriptSemantics.Field(p.Name));
-//                }
-//            }
+                foreach (var p in type.Properties.Where(p => IsAutoProperty(p) == true))
+                {
+                    base.ReserveMemberName(p.DeclaringTypeDefinition, p.Name, false);
+                    base.SetPropertySemantics(p, PropertyScriptSemantics.Field(p.Name));
+                }
+            }
 
-//            if (AttributeReader.HasAttribute<EntityAttribute>(type))
-//            {
+            if (AttributeReader.HasAttribute<EntityAttribute>(type))
+            {
 
-//                foreach (var p in type.Properties.Where(p => IsAutoProperty(p) == true && p.FullName == "JayData.EntitySet"))
-//                {
-//                    base.ReserveMemberName(p.DeclaringTypeDefinition, p.Name, false);
-//                    base.SetPropertySemantics(p, PropertyScriptSemantics.Field(p.Name));
-//                }
-//            }
-//            base.Prepare(type);
-//        }
+                foreach (var p in type.Properties.Where(p => IsAutoProperty(p) == true && p.FullName == "JayData.EntitySet"))
+                {
+                    base.ReserveMemberName(p.DeclaringTypeDefinition, p.Name, false);
+                    base.SetPropertySemantics(p, PropertyScriptSemantics.Field(p.Name));
+                }
+            }
+            base.Prepare(type);
+        }
 
-//        public JsExpression ResolveTypeParameter(ITypeParameter tp)
-//        {
-//            var typeName = JsExpression.Identifier(_namer.GetTypeParameterName(tp));
-//            return typeName;
-//        }
+        public JsExpression ResolveTypeParameter(ITypeParameter tp)
+        {
+            var typeName = JsExpression.Identifier(_namer.GetTypeParameterName(tp));
+            return typeName;
+        }
 
-//        public JsExpression EnsureCanBeEvaluatedMultipleTimes(JsExpression expression, IList<JsExpression> expressionsThatMustBeEvaluatedBefore)
-//        {
-//            throw new NotImplementedException();
-//        }
-//    }
-//}
+        public JsExpression EnsureCanBeEvaluatedMultipleTimes(JsExpression expression, IList<JsExpression> expressionsThatMustBeEvaluatedBefore)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<JsType> Rewrite(IEnumerable<JsType> types)
+        {
+            var clazz = types.First() as JsClass;
+        }
+    }
+}
 
---
+++ SaltarelleJayData.Example/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SaltarelleJayData.Example
+{
+    public class A
+    {
+        public A(int a)
+        {
+            AInt = a;
+        }
+
+        public int AInt { get; set; }
+    }
+
+    public class B : A
+    {
+        public B(int a, int b) : base(a)
+        {
+            BInt = b;
+        }
+
+        public int BInt { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var b = new B(2, 3);
+        }
+    }
+}
